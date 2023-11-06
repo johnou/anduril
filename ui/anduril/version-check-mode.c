af7b6df101205f9c20f2b8c635ae9d809ffeec1c
Johno Crawford <johno@sulake.com>
2023-11-06 01:24:39 +0100

    Morse code fun.

--- ui/anduril/version-check-mode.c
+++ ui/anduril/version-check-mode.c
@@ -6,6 +6,54 @@
 
 #include "anduril/version-check-mode.h"
 
+#define MORSE_DIT_LENGTH 400
+
+// 'A' to 'Z'
+const char *morse_code[] = {
+        ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..",
+        ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.",
+        "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."
+    };
+
+#define NUM_PHRASES 10
+
+const char *phrases[NUM_PHRASES] = {
+    "Ciao nemico",
+    "Stato bello  per te",
+    "Ti odio",
+    "Zuccone",
+    "Inutile",
+    "Fannullone",
+    "Amazing",
+    "Incredible",
+    "Non mi fa spawnare",
+    "Numero uno"
+};
+
+const char* get_random_phrase() {
+    uint8_t index = pseudo_rand() % NUM_PHRASES;
+    return phrases[index];
+}
+
+char to_upper(char ch) {
+    if (ch >= 'a' && ch <= 'z') {
+        return ch - 32;
+    }
+    return ch;
+}
+
+void blink_morse(const char *morse) {
+    for (uint8_t i = 0; morse[i]; i++) {
+        set_level(memorized_level);
+        nice_delay_ms(MORSE_DIT_LENGTH);
+        if (morse[i] == '-') {
+            nice_delay_ms(MORSE_DIT_LENGTH * 4);
+        }
+        set_level(0);
+        nice_delay_ms(MORSE_DIT_LENGTH * 4);
+    }
+}
+
 // empty state; logic is handled in FSM loop() instead
 uint8_t version_check_state(Event event, uint16_t arg) {
     return EVENT_NOT_HANDLED;
@@ -13,19 +61,16 @@ uint8_t version_check_state(Event event, uint16_t arg) {
 
 // this happens in FSM loop()
 inline void version_check_iter() {
-    for (uint8_t i=0; i<sizeof(version_number)-1; i++) {
-        uint8_t digit = pgm_read_byte(version_number + i) - '0';
-        if (digit < 10) blink_digit(digit);
-        else {  // "buzz" for non-numeric characters
-            for(uint8_t frame=0; frame<25; frame++) {
-                set_level((frame&1) << 5);
-                nice_delay_ms(16);
-            }
-            nice_delay_ms(BLINK_SPEED * 8 / 12);
+    const char *phrase = get_random_phrase();
+    for (uint8_t i = 0; phrase[i]; i++) {
+        char c = to_upper(phrase[i]);
+        if (c >= 'A' && c <= 'Z') {
+            const char *morse = morse_code[c - 'A'];
+            blink_morse(morse);
+            nice_delay_ms(MORSE_DIT_LENGTH * 10);
+        } else if (c == ' ') {
+            nice_delay_ms(MORSE_DIT_LENGTH * 20);
         }
-        nice_delay_ms(300);
     }
-
     set_state_deferred(off_state, 0);
 }
-
